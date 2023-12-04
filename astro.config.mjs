fbfd47d0748cd921b2ac21bd0fb3ec441348e538
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-12-04 13:00:27 +0530

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-import node from '@astrojs/node'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
+import vercel from '@astrojs/vercel/serverless'
 
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
   integrations: [tailwind()],
 })
